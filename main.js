246f346eabee53aedfac934a5d4da69b93426e1e
Bruno Annunciato <brunobannunciato@gmail.com>
2018-06-15 18:48:49 -0300

    add winner team mark and location column

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
 var api = 'http://worldcup.sfg.io/matches/today/?by_date=DESC',
     table = document.querySelector('.table')
 
-function addGame(status, homeTeam, homeGoal, awayGoal, awayTeam) {
+function addGame(status, location, homeTeam, homeGoal, awayGoal, awayTeam) {
     var statusTd = document.createElement('td'),
         statusValue = statusTd.textContent = status
+
+    var locationTd = document.createElement('td'),
+    locationValue = locationTd.textContent = location
 
     var homeTeamTd = document.createElement('td'),
         homeTeamValue = homeTeamTd.textContent = homeTeam
     var homeGoalTd = document.createElement('td'),
         homeGoalValue = homeGoalTd.textContent = homeGoal
+
+        homeTeamTd.classList.add('teamTd')
     
     var versusTd = document.createElement('td'),
         versusValue = versusTd.textContent = '-'
@@ -18,11 +23,14 @@ function addGame(status, homeTeam, homeGoal, awayGoal, awayTeam) {
     var awayTeamTd = document.createElement('td'),
         awayTeamValue = awayTeamTd.textContent = awayTeam
 
+        awayTeamTd.classList.add('teamTd')
+
     var line = document.createElement('tr')
 
     line.classList.add('jogo')
 
     line.appendChild(statusTd)
+    line.appendChild(locationTd)
     line.appendChild(homeTeamTd)
     line.appendChild(homeGoalTd)
     line.appendChild(versusTd)
@@ -44,7 +52,9 @@ function fillTable() {
     .then(function(response) {
       response.json().then(function(games) {
           games.forEach(function(game){
+              console.log(game)
                 var status = game.status,
+                    location = game.location,
                     homeTeam = game.home_team.country,
                     homeGoals = game.home_team.goals,
                     awayTeam = game.away_team.country,
@@ -54,14 +64,21 @@ function fillTable() {
                         var status = game.time
                     }
 
-                    console.log(status)
-                    addGame(status, homeTeam, homeGoals, awayGoals, awayTeam)
+                    addGame(status, location, homeTeam, homeGoals, awayGoals, awayTeam)
+
+                    var winner = game.winner
+
+                    document.querySelectorAll('.teamTd').forEach(function(td) {
+                        if (td.textContent == winner) {
+                            td.classList.add('winner')
+                        }
+                    })
+
           })
       })
     })
-    console.log('working..')
 }
 
 fillTable();
-setInterval(clearTable, 29500)
+setInterval(clearTable, 30000)
 setInterval(fillTable, 30000)
